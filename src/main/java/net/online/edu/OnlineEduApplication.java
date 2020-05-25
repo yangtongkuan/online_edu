e4d60fc01f5763dc6ccd9c2f71269f192e5bdd6d
yangtongkuan <yangtongkuan@qq.com>
2020-05-25 21:59:51 +0800

    异步任务 定时任务

--- src/main/java/net/online/edu/OnlineEduApplication.java
+++ src/main/java/net/online/edu/OnlineEduApplication.java
@@ -3,6 +3,8 @@ package net.online.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,6 +15,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
  */
 @SpringBootApplication
 @ServletComponentScan
+@EnableScheduling
+@EnableAsync
 public class OnlineEduApplication {
 
     public static void main(String[] args) {
